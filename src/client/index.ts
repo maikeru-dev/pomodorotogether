c399f3ea0e71781c1f769a3a0bd3122665214cd4
maikeru-dev <dprofilo7@gmail.com>
2025-03-03 03:33:01 +0000

    [clean] rm dump code

--- src/client/index.ts
+++ src/client/index.ts
@@ -6,13 +6,7 @@ import {
 } from "../common/interfaces";
 import { formMessageBlock, formPomodoroState } from "../common/functions";
 
-let hamburgerDOM: HTMLElement;
-let closeBurgerDOM: HTMLElement;
-let settingsDOM: HTMLElement;
-let mainPageDOM: HTMLElement;
-let currentSettings: HTMLElement;
-
-let currentState: PomodoroState = {};
+let currentState: boolean = false; // stop or start
 
 class PomodoroStateImpl implements PomodoroState {
   timeDOM: HTMLElement;
@@ -40,21 +34,11 @@ class PomodoroStateImpl implements PomodoroState {
 }
 
 const socket = new WebSocket("ws://" + window.location.host + "/api/v1/");
-function getConfig() {}
-
-function generateSocketMessage(event: PomodoroEvent): string {
-  let state = formPomodoroState(getCurrentTime(), getCurrentCycle());
-  let config = getConfig();
-
-  let messageBlock = formMessageBlock(state, event, config);
-
-  return JSON.stringify(messageBlock);
-}
 
 socket.addEventListener("open", (event) => {
   console.log("Opened it!");
 
-  socket.send(formMessageBlock());
+  // socket.send();
 });
 socket.addEventListener("close", (event) => {
   console.log(event.reason);
@@ -63,76 +47,3 @@ socket.addEventListener("close", (event) => {
 socket.addEventListener("message", (event) => {
   console.log("Message from server ", event.data);
 });
-
-function isSettingsCustom() {
-  return false;
-}
-
-function writeSettings() {
-  return;
-}
-
-function saveSettings() {
-  return;
-}
-function settingsDeepEqual() {
-  return true;
-}
-function loadSettingsFromLocalStorage() {
-  return;
-}
-
-function saveSettingsToLocalStorage() {
-  return;
-}
-
-function openSettings() {
-  // Load settings
-  if (!isSettingsCustom()) {
-    // write to DOM
-    let newSettings = loadSettingsFromLocalStorage();
-    if (newSettings) currentSettings = newSettings;
-    writeSettings(currentSettings);
-  }
-
-  // Update styles
-  hamburgerDOM.style.display = "none";
-  closeBurgerDOM.style.display = "block";
-
-  mainPageDOM.style.display = "none";
-  settingsDOM.style.display = "flex";
-}
-function closeSettings() {
-  let newSettings;
-  // save from DOM
-  saveSettings(newSettings);
-  if (!settingsDeepEqual(currentSettings, newSettings)) {
-    localStorage.setItem("customSettings", "somevalue");
-    currentSettings = newSettings;
-    saveSettingsToLocalStorage(currentSettings);
-    applySettings(newSettings);
-  }
-
-  closeBurgerDOM.style.display = "none";
-  hamburgerDOM.style.display = "block";
-
-  mainPageDOM.style.display = "block";
-  settingsDOM.style.display = "none";
-}
-
-function testConn() {}
-
-function init() {
-  closeBurgerDOM = document.getElementById("closeBurger");
-  hamburgerDOM = document.getElementById("openHamburger");
-  settingsDOM = document.getElementById("settings");
-  mainPageDOM = document.getElementById("mainPage");
-}
-
-document.addEventListener("DOMContentLoaded", init);
-function getCurrentTime(): any {
-  throw new Error("Function not implemented.");
-}
-function getCurrentCycle(): any {
-  throw new Error("Function not implemented.");
-}
