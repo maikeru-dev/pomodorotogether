da341614cfeb12bf0b8c32868ddf57390eb30d89
maikeru-dev <dprofilo7@gmail.com>
2025-03-06 03:37:08 +0000

    [add] mutexes

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,17 +1,34 @@
-import bodyParser from "body-parser";
+// import bodyParser from "body-parser";
 import { app } from "../lib/app/index.cjs";
 import express from "express";
 import expressWs from "express-ws";
 import { MessageBlock } from "../common/interfaces.js";
+import { WebSocket } from "ws";
+import {
+  Mutex,
+  MutexInterface,
+  Semaphore,
+  SemaphoreInterface,
+  withTimeout,
+} from "async-mutex";
 
 expressWs(app);
 
 const router = express.Router();
+const clients = new Set<WebSocket>();
+const mutex = new Mutex();
 
 router.ws("/", (ws, req) => {
+  clients.add(ws);
+  ws.on("open", () => {
+    console.log("New client connected");
+  });
   ws.on("message", (msg) => {
     let msgBlock = JSON.parse(msg.toString()) as MessageBlock;
     console.log(msgBlock.event);
+  });
+  ws.on("close", () => {
+    clients.delete(ws);
   });
 });
 
